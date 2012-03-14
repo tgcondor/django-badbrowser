6d2bf1942365f736db5d7f7bcc5c3ed0df74eb12
Adam Charnock <adam@omniwiki.co.uk>
2012-03-14 00:47:54 +0000

    Now using setup() from distutils where available

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 import os
 
 from django_badbrowser import __version__
